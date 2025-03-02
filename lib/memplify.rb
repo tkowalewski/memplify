0106bbe1adf84402dc4197ac00f975b326582a15
Tomasz Kowalewski <me@tkowalewski.pl>
2025-03-02 12:02:20 +0100

    Initial commit

---
+++ lib/memplify.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "memplify/version"
+
+module Memplify
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/memplify/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Memplify
+  VERSION = "0.1.0"
+end
